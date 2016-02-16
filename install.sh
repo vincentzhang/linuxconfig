db5399c26e0b072e277301a8df082bfffb0e9c7d
vincentzhang <vincent.zhang@ualberta.ca>
2016-02-16 00:14:50 -0700

    fix path

--- install.sh
+++ install.sh
@@ -6,10 +6,9 @@ cd ~/.vim
 git clone https://github.com/ctrlpvim/ctrlp.vim.git bundle/ctrlp.vim
 # :helptags ~/.vim/bundle/ctrlp.vim/doc
 # sensible
-git clone git://github.com/tpope/vim-sensible.git
+git clone git://github.com/tpope/vim-sensible.git bundle/vim-sensible
 # powerline
 pip install powerline-status
-mkdir ~/repo && cd ~/repo
+mkdir -p ~/repo && cd ~/repo
 git clone git@github.com:powerline/fonts.git
-cd fonts
-./install.sh
+cd fonts && ./install.sh
